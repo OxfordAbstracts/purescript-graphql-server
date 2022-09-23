7a746e6d8740ae6b083905ef23ff59929471714c
Rory Campbell <roryc89@googlemail.com>
2022-09-23 22:14:31 +0100

    adds result to data:

--- src/GraphQL/Resolver/HandleQuery.purs
+++ src/GraphQL/Resolver/HandleQuery.purs
@@ -3,9 +3,17 @@ module GraphQL.Resolver.HandleQuery where
 import Prelude
 
 import Data.Argonaut (Json)
+import Data.Maybe (Maybe(..))
+import GraphQL.Parser.AST as AST
+import GraphQL.Resolver.JsonResolver (Resolver, resolve)
 import GraphQL.Resolver.ToResolver (class ToJsonResolver)
-import GraphQL.Resolver.JsonResolver (Resolver)
+import Unsafe.Coerce (unsafeCoerce)
 
+
+handleOperationDefinition :: forall m. Applicative m =>  Resolver m  -> AST.OperationDefinition -> m Json
+handleOperationDefinition resolver = case _ of
+  AST.OperationDefinition_SelectionSet selectionSet -> resolve resolver (Just selectionSet)
+  AST.OperationDefinition_OperationType _ -> unsafeCoerce unit
 
 -- handleQuery :: forall m a . Resolver m -> String -> m Json
 -- handleQuery resolver query = do
--- src/GraphQL/Resolver/Result.purs
+++ src/GraphQL/Resolver/Result.purs
@@ -2,13 +2,14 @@ module GraphQL.Resolver.Result where
 
 import Prelude
 
-import Data.Argonaut (Json, stringify)
+import Data.Argonaut (Json, fromArray, fromObject, jsonNull, stringify)
+import Data.Array as Array
 import Data.Generic.Rep (class Generic)
 import Data.List (List)
-import Data.Maybe (Maybe)
-import Data.Tuple (Tuple)
+import Data.Maybe (Maybe, maybe)
+import Data.Tuple (Tuple(..))
+import Foreign.Object as Object
 import GraphQL.Server.GqlError (ResolverError)
-
 
 data Result
   = ResultLeaf Json
@@ -27,3 +28,14 @@ instance Show Result where
     ResultObject fields -> "(ResultObject " <> show fields <> ")"
     ResultList items -> "(ResultList " <> show items <> ")"
     ResultNullable maybeResult -> "(ResultNullable " <> show maybeResult <> ")"
+
+resultToData :: Result -> Json
+resultToData = case _ of
+  ResultLeaf json -> json
+  ResultError _err -> jsonNull
+  ResultObject fields -> fromObject $ Object.fromFoldable $
+    fields <#> \(Tuple name result) -> Tuple name $ resultToData result
+  ResultList items -> fromArray $ Array.fromFoldable $ map resultToData items
+  ResultNullable maybeResult -> maybe jsonNull resultToData maybeResult
+
+-- resultToError :: Result -> Array 
