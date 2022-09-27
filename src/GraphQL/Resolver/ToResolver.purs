e5828540bdb0913fafc843bcd5c53118b305aa09
Rory Campbell <roryc89@googlemail.com>
2022-09-27 20:53:39 +0100

    remove printGql

--- src/GraphQL/Resolver/ToResolver.purs
+++ src/GraphQL/Resolver/ToResolver.purs
@@ -16,75 +16,61 @@ import GraphQL.Resolver.JsonResolver as JsonResolver
 import GraphQL.Resolver.Resolver.GqlObject (GqlObj(..), GqlNew)
 import GraphQL.Server.GqlError (ResolverError(..))
 import Heterogeneous.Folding (class FoldingWithIndex, class HFoldlWithIndex, hfoldlWithIndex)
-import Type.Proxy (Proxy(..))
+import Type.Proxy (Proxy)
 
 class ToResolver a m where
   toResolver :: a -> JsonResolver.Resolver m
-  printGql :: Proxy (m a) -> String
 
 instance Applicative m => ToResolver Boolean m where
   toResolver a = Node $ pure $ encodeJson a
-  printGql _ = "Boolean"
 
 instance Applicative m => ToResolver Int m where
   toResolver a = Node $ pure $ encodeJson a
-  printGql _ = "Int"
 
 instance Applicative m => ToResolver Number m where
   toResolver a = Node $ pure $ encodeJson a
-  printGql _ = "Float"
 
 instance Applicative m => ToResolver String m where
   toResolver a = Node $ pure $ encodeJson a
-  printGql _ = "String"
 
 instance Applicative m => ToResolver Json m where
   toResolver a = Node $ pure $ encodeJson a
-  printGql _ = "Json"
 
 instance (Applicative m, ToResolver a m) => ToResolver (List a) m where
   toResolver a = ListResolver $ map toResolver a
-  printGql _ = "List"
 
 instance (Applicative m, ToResolver a m) => ToResolver (Array a) m where
   toResolver a = ListResolver $ map toResolver $ List.fromFoldable a
-  printGql _ = "List"
-
 
 instance
   ( Applicative m
   , HFoldlWithIndex ToResolverProps (FieldMap m) { | r } (FieldMap m)
-  , IsSymbol name
   ) =>
   ToResolver (GqlObj name { | r }) m where
   toResolver (GqlObj a) = Fields
     { fields: makeFields a
     }
-  printGql _ = reflectSymbol (Proxy :: Proxy name)
-
--- instance
---   ( Applicative m
---   , HFoldlWithIndex ToResolverProps (FieldMap m) { | r } (FieldMap m)
---   ) =>
---   ToResolver ({ | r }) m where
---   toResolver a = Fields
---     { fields: makeFields a
---     }
---   printGql _ = "List"
 
 instance
   ( Applicative m
-  , Newtype n { | r } 
+  , HFoldlWithIndex ToResolverProps (FieldMap m) { | r } (FieldMap m)
+  ) =>
+  ToResolver ({ | r }) m where
+  toResolver a = Fields
+    { fields: makeFields a
+    }
+
+instance
+  ( Applicative m
+  , Newtype n { | r }
   , HFoldlWithIndex ToResolverProps (FieldMap m) { | r } (FieldMap m)
   ) =>
   ToResolver (GqlNew n) m where
   toResolver = unwrap >>> unwrap >>> \a -> Fields
     { fields: makeFields a }
-  printGql _ = "TODO"
 
 instance (Functor m, EncodeJson a) => ToResolver (GqlIo m a) (GqlIo m) where
   toResolver m = Node $ map encodeJson m
-  printGql _ = "TODO"
 
 makeFields
   :: forall r m
