6139982b26f3f4cd4be400964bdfb127698bfa33
Rory Campbell <roryc89@googlemail.com>
2022-10-16 11:33:49 +0100

    adds GqlNullable module

---
+++ src/GraphQL/Server/Schema/Introspection/GqlNullable.purs
@@ -0,0 +1,13 @@
+module GraphQL.Server.Schema.Introspection.GqlNullable where
+
+import Data.Maybe (Maybe)
+import Type.Proxy (Proxy)
+
+class GqlNullable :: forall k. k -> Constraint
+class GqlNullable a where
+  isNullable :: Proxy a -> Boolean
+
+instance GqlNullable (Maybe a) where
+  isNullable _ = true
+else instance GqlNullable a where
+  isNullable _ = false
